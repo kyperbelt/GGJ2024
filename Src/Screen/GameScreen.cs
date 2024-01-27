b1e932af02d500eb99312edac23af492d2e62cfc
kyperbelt <jonathancamarenadev@gmail.com>
2024-01-27 02:25:46 -0800

    screen management

---
+++ Src/Screen/GameScreen.cs
@@ -0,0 +1,96 @@
+using Godot;
+
+[GlobalClass]
+public partial class GameScreen : Node
+{
+    [Signal]
+    public delegate void ScreenPausedEventHandler();
+
+    [Signal]
+    public delegate void ScreenResumedEventHandler();
+
+    [Export]
+    private bool _pausePreviousScreen = false;
+
+    [ExportGroup("Config")]
+    [Export(PropertyHint.Dir)]
+    private string _screenDirectory = "res://Src/Screen";
+
+    private ScreenManager _screenManager;
+
+    public override void _Ready()
+    {
+    }
+
+    public void SetScreen(string screenName)
+    {
+        var screen = GetGameScreenFromName(screenName);
+        if (screen != null)
+        {
+            _screenManager.SetScreen(screen);
+        }
+    }
+
+    public void PushScreen(string screenName)
+    {
+        var screen = GetGameScreenFromName(screenName);
+        if (screen != null)
+        {
+
+            _screenManager.PushScreen(screen);
+        }
+    }
+
+    public void PopScreen()
+    {
+        _screenManager.PopScreen();
+    }
+
+    public void SetScreenPaused(bool paused)
+    {
+        if (paused)
+        {
+            EmitSignal(SignalName.ScreenPaused);
+        }
+        else
+        {
+            EmitSignal(SignalName.ScreenResumed);
+        }
+    }
+
+    public bool IsPausePreviousScreen()
+    {
+        return _pausePreviousScreen;
+    }
+
+    public void SetScreenManager(ScreenManager manager)
+    {
+        _screenManager = manager;
+    }
+
+    private GameScreen GetGameScreenFromName(string name)
+    {
+        var scene = GD.Load<PackedScene>($"{_screenDirectory}/{name}.tscn");
+        GD.Print($"Loading screen from {_screenDirectory}/{name}.tscn");
+        if (scene == null)
+        {
+            GD.Print($"Loading screen from {_screenDirectory}/{name}.scn");
+            scene = GD.Load<PackedScene>($"{_screenDirectory}/{name}.scn");
+        }
+
+        if (scene == null)
+        {
+            GD.PushError($"GameScreen[{name}] not found at in Dir[{_screenDirectory}].");
+            return null;
+        }
+
+        var instance = scene.Instantiate();
+        if (instance is GameScreen screen)
+        {
+            return screen;
+        }
+
+        GD.PushError($"screen[{name}] was not a valid GameScreen name. tip: Do not include the extension (tscn).");
+        return null;
+    }
+}
---
+++ Src/Screen/ScreenManager.cs
@@ -0,0 +1,110 @@
+using System.Collections.Generic;
+using Godot;
+
+[GlobalClass]
+[Tool]
+public partial class ScreenManager : Node
+{
+    private readonly Stack<GameScreen> _screenStack;
+
+    public ScreenManager()
+    {
+        _screenStack = new Stack<GameScreen>();
+    }
+
+    public override void _Ready()
+    {
+        // GUARD
+        if (Engine.IsEditorHint())
+        {
+            return;
+        }
+
+        AddToGroup("ScreenManager");
+
+	// Look for the first GameScreen that is a child of this manager
+	// to use as a starting screen.
+        var startingScreen = GetStartingScreen();
+
+        if (startingScreen != null)
+        {
+            PushScreen(startingScreen);
+        }
+        else
+        {
+		GD.PushError("No Starting Game Screen Detected");
+        }
+    }
+
+    private GameScreen GetStartingScreen()
+    {
+        for (int i = 0; i < GetChildCount(); i++)
+        {
+            if (GetChild(i) is GameScreen screen)
+                return screen;
+        }
+        return null;
+    }
+
+    public override string[] _GetConfigurationWarnings()
+    {
+        var warnings = new List<string>();
+
+        if (GetStartingScreen() == null)
+        {
+            warnings.Add("No starting screen found.");
+        }
+
+        return warnings.ToArray();
+    }
+
+    public void PushScreen(GameScreen screen)
+    {
+        if (_screenStack.Count > 0 && screen.IsPausePreviousScreen())
+        {
+            _screenStack.Peek().SetScreenPaused(true);
+            _screenStack.Peek().ProcessMode = ProcessModeEnum.Disabled;
+            GD.Print($"'{_screenStack.Peek().Name}' has been paused.");
+        }
+
+        _screenStack.Push(screen);
+        screen.SetScreenManager(this);
+        if (screen.GetParent() == null)
+        {
+            AddChild(screen);
+        }
+
+        GD.Print($"'{screen.Name}' pushed to the top of the Screen Stack!");
+    }
+
+    public void PopScreen()
+    {
+        if (_screenStack.Count == 0)
+        {
+            GD.Print("Unable to pop when there are no screens on the Screen Stack.");
+            return;
+        }
+
+        var screen = _screenStack.Pop();
+        GD.Print($"'{screen.Name}' has been popped from the Screen Stack!");
+        if (screen.IsPausePreviousScreen() && _screenStack.Count > 0)
+        {
+            _screenStack.Peek().SetScreenPaused(false);
+            _screenStack.Peek().ProcessMode = ProcessModeEnum.Inherit;
+            GD.Print($"'{_screenStack.Peek().Name}' has been unpaused.");
+        }
+
+        screen.QueueFree();
+    }
+
+    public void SetScreen(GameScreen screen)
+    {
+        while (_screenStack.Count > 0)
+        {
+            PopScreen();
+        }
+
+        PushScreen(screen);
+    }
+
+}
