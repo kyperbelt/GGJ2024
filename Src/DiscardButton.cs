7a51941b37ae3975275b7bd7182de2852f7a36fe
Greg Harrison <harrison.greg.c@gmail.com>
2024-01-27 21:37:18 -0800

    Discard Pile ready

---
+++ Src/DiscardButton.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+[Tool]
+public partial class DiscardButton : Control
+{
+	int discardPileNumber = 0;
+
+	[Export]
+	public int DiscardPileNumber
+	{
+		get { return discardPileNumber; }
+		set
+		{
+			discardPileNumber = value;
+			if (_label == null) return;
+			_label.Text = value.ToString();
+		}
+	}
+
+	private Label _label;
+
+	public override void _Ready()
+	{
+		_label = GetNode<Label>("DiscardPileLabel");
+	}
+}
